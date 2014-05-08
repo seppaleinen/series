ba33a2c434c857657435fc934c3287572026c6ca
david eriksson <davideriksson@swedenmail.com>
2014-05-08 19:27:51 +0200

    mer tester

--- web/war/wicket/src/main/java/StartPage.java
+++ web/war/wicket/src/main/java/StartPage.java
@@ -1,8 +1,6 @@
 import MediaFinder.FileFinder.FileFinder;
 import MediaFinder.Finder;
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.ajax.AjaxEventBehavior;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
@@ -11,6 +9,7 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.PropertyModel;
 
 import java.util.ArrayList;
@@ -19,81 +18,71 @@ import java.util.List;
 public class StartPage extends WebPage {
     private static final long serialVersionUID = 1L;
 
-    private static final String FORM_ID = "form";
-    private static final String DIRECTORY_ID = "directory";
-    private static final String LISTVIEW_ID = "listView";
-    private static final String LISTVIEWCONTAINER_ID = "listViewContainer";
-    private static final String NAME_ID = "name";
-    private static final String SEARCH_BUTTON_ID = "searchButton";
-
+    public static final String FORM_ID = "form";
+    public static final String DIRECTORY_ID = "directory";
+    public static final String LISTVIEW_ID = "listView";
+    public static final String LISTVIEWCONTAINER_ID = "listViewContainer";
+    public static final String NAME_ID = "name";
+    public static final String SEARCH_BUTTON_ID = "searchButton";
 
     private Form form;
     private TextField<String> directory;
     private Button searchMediaButton;
     private ListView<String> listView;
     private WebMarkupContainer listViewContainer;
+
     private List<String> results = new ArrayList<>();
     private ModelObject modelObject = new ModelObject();
 
     public StartPage(PageParameters pageParameters) {
+        setCompountPropertyModel();
         instantiateComponents();
         addComponents();
     }
 
-    private void instantiateComponents(){
+    private void setCompountPropertyModel(){
+        setDefaultModel(new CompoundPropertyModel<ModelObject>(modelObject));
+    }
 
+    private void instantiateComponents(){
         form = new Form(FORM_ID);
-        directory = new TextField<>(DIRECTORY_ID, new PropertyModel<String>(modelObject, "directory"));
+        directory = new TextField<>(DIRECTORY_ID);
         listViewContainer = new WebMarkupContainer(LISTVIEWCONTAINER_ID);
-        listViewContainer.setMarkupId(LISTVIEWCONTAINER_ID);
-        listViewContainer.setOutputMarkupId(true);
-        listView = new ListView<String>(LISTVIEW_ID, results) {
+        listView = createListView();
+        searchMediaButton = createSearchMediaButton();
+    }
+
+    private void addComponents(){
+        listViewContainer.add(listView);
+        form.add(listViewContainer);
+        form.add(searchMediaButton);
+        form.add(directory);
+        add(form);
+    }
+
+    private ListView<String> createListView(){
+        return new ListView<String>(LISTVIEW_ID, results) {
             @Override
             protected void populateItem(ListItem<String> resultItem) {
                 String result = resultItem.getModelObject();
                 resultItem.add(new Label(NAME_ID, result));
             }
         };
-        listViewContainer.add(listView);
-        searchMediaButton = new Button(SEARCH_BUTTON_ID){
+    }
+
+    private Button createSearchMediaButton(){
+        return new Button(SEARCH_BUTTON_ID){
             @Override
             public void onSubmit(){
                 Finder mediaFinder = new FileFinder();
-                System.out.println("DIRECTORYTEXT: " + modelObject.getDirectory());
                 if(modelObject.getDirectory()!= null && !modelObject.getDirectory().isEmpty()){
                     List<String> mediaFinderResults = mediaFinder.searchDirectory(modelObject.getDirectory());
                     results.clear();
                     for(String resultString : mediaFinderResults){
                         results.add(resultString);
                     }
-                    //ajaxRequestTarget.addComponent(listViewContainer, LISTVIEWCONTAINER_ID);
                 }
             }
         };
-        //searchMediaButton.add(getClickEvent());
-    }
-
-    private AjaxEventBehavior getClickEvent(){
-        return new AjaxEventBehavior("onclick") {
-            @Override
-            protected void onEvent(AjaxRequestTarget ajaxRequestTarget) {
-                Finder mediaFinder = new FileFinder();
-                System.out.println("DIRECTORYTEXT: " + modelObject.getDirectory());
-                if(modelObject.getDirectory()!= null && !modelObject.getDirectory().isEmpty()){
-                    List<String> mediaFinderResults = mediaFinder.searchDirectory("/Users/shaman_king_2000/Downloads/Serier");
-                    for(String resultString : mediaFinderResults){
-                        results.add(resultString);
-                    }
-                    ajaxRequestTarget.addComponent(listViewContainer, LISTVIEWCONTAINER_ID);
-                }
-            }
-        };
-    }
-
-    private void addComponents(){
-        form.add(listViewContainer);
-        form.add(searchMediaButton);
-        form.add(directory);
-        add(form);
     }
 }
--- web/war/wicket/src/test/java/StartPageTest.java
+++ web/war/wicket/src/test/java/StartPageTest.java
@@ -1,29 +1,50 @@
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.util.tester.FormTester;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Locale;
 
-public class StartPageTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
+public class StartPageTest {
+    private static final String MEDIA = "/Media";
     private WicketTester tester;
+    private StartPage startPage;
 
     @Before
     public void setUp() throws Exception {
         tester = new WicketTester();
+        startPage = new StartPage(new PageParameters());
         tester.getWicketSession().setLocale(Locale.ENGLISH);
     }
 
     @Test
     public void testRenderPage() {
-        StartPage startPage = new StartPage(new PageParameters());
         tester.startPage(startPage);
         tester.assertNoErrorMessage();
         tester.assertRenderedPage(StartPage.class);
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     public void testPressButton(){
+        tester.startPage(startPage);
+        FormTester formTester = tester.newFormTester("form");
+        formTester.setValue(StartPage.DIRECTORY_ID, getFile(MEDIA));
+        formTester.setValue(StartPage.SEARCH_BUTTON_ID, "");
+        formTester.submit();
+        tester.assertNoErrorMessage();
+        String listViewId = StartPage.FORM_ID + ":" + StartPage.LISTVIEWCONTAINER_ID + ":" + StartPage.LISTVIEW_ID;
+        ListView<String> listView = (ListView<String>) tester.getComponentFromLastRenderedPage(listViewId);
+        assertEquals("ListView size should be as expected", listView.size(), 6);
+    }
+
+    private String getFile(String directory){
+        return this.getClass().getResource(directory).getFile().replace("%20", " ");
     }
 }
